8ee2563a29ef660f7514b95c7d777ff3ee58a98a
Naveen Singh <snaveen935@gmail.com>
2026-02-08 00:11:29 +0530

    build: add compose compiler plugin to root project

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,5 @@ plugins {
     alias(libs.plugins.android).apply(false)
     alias(libs.plugins.ksp).apply(false)
     alias(libs.plugins.detekt).apply(false)
+    alias(libs.plugins.compose.compiler).apply(false)
 }
